c5a8daa16fb7b41bcc1e3999d330773230dabeba
Oshadhi <oshadhiranawaka@gmail.com>
2026-01-27 13:40:49 +0530

    feat(auth): enhance EnterEmailScreen layout and styling

--- mobile/src/app/authScreens/enterEmail.tsx
+++ mobile/src/app/authScreens/enterEmail.tsx
@@ -1,27 +1,44 @@
 import React from 'react';
 import { AppText } from '@/components/AppText';
-import { TextInput, View } from 'react-native';
+import { Pressable, TextInput, View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
 
 export default function EnterEmailScreen() {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.contentContainer}>
         <AppText
-          variant="screenTitle"
+          variant="sectionTitle"
           color="primary"
-          style={{ textAlign: 'left' }}
+          style={{ textAlign: 'left',paddingLeft: 10 }}
         >
           Reset Your Password 🔑
         </AppText>
-        <AppText variant="body" style={{ textAlign: 'left', marginTop: 10, marginLeft: 5 }}>
+        <AppText
+          variant="body"
+          style={{ textAlign: 'left', marginTop: 20, marginLeft: 5, paddingLeft: 5 }}
+        >
           Please enter your email address below and we will send an OTP code to
           reset your password.
-              </AppText>
+        </AppText>
 
-        <AppText variant="listHeader" style = {{padding: 20, textAlign: 'left'}}> Email</AppText>
+        <View>
+          <AppText
+            variant="listHeader"
+            style={{ paddingTop: 50, paddingBottom:10, marginLeft: 10, textAlign: 'left' }}
+          >
+            {' '}
+            Email
+          </AppText>
 
-              <TextInput> </TextInput>
+          <TextInput style={styles.input} placeholder='example@gmail.com'/>
+        </View>
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.button}>
+          <AppText variant='listHeader' style={{color: "#FFFFFF"}}> Continue </AppText>
+        </Pressable>
       </View>
     </View>
   );
@@ -30,13 +47,33 @@ export default function EnterEmailScreen() {
 const styles = StyleSheet.create((theme, rt) => ({
   container: {
     flex: 1,
-    backgroundColor: theme.background.secondary,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
+    backgroundColor: "#FFFFFF",
+    paddingTop: 100,
   },
-  textContainer: {
+  contentContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 20,
   },
+  buttonContainer: {
+    paddingBottom: 60,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 6,
+    padding: 10,
+    marginTop: 2,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  button: {
+    alignItems: 'center',
+    paddingTop: 15,
+    paddingBottom: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 25,
+    backgroundColor: "#9500FF",
+  }
 }));
