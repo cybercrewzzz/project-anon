6adf884e4af09dd4c91462d21a9e6c3090d1fa94
ubddprasanna <ubddprasannapro@gmail.com>
2026-01-30 17:05:17 +0530

    style: format code in _layout.tsx for improved readability

--- mobile/src/app/StartedScreens/_layout.tsx
+++ mobile/src/app/StartedScreens/_layout.tsx
@@ -2,16 +2,16 @@ import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-      <Stack>
-        <Stack.Screen
-          name="selectLanguage"
-          options={{
-            headerShown: false,
-            title: '',
-            headerTitleAlign: 'center',
-            headerTransparent: true,
-          }}
-        />
-      </Stack>
+    <Stack>
+      <Stack.Screen
+        name="selectLanguage"
+        options={{
+          headerShown: false,
+          title: '',
+          headerTitleAlign: 'center',
+          headerTransparent: true,
+        }}
+      />
+    </Stack>
   );
 }
