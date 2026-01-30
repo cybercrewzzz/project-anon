9d0c8c4c65cd8af01dee2ee879d108df0fa601dd
Oshadhi <oshadhiranawaka@gmail.com>
2026-01-30 12:19:10 +0530

    feat(auth): update EnterEmailScreen layout and navigation to reset password

--- mobile/src/app/authScreens/enterEmail.tsx
+++ mobile/src/app/authScreens/enterEmail.tsx
@@ -3,41 +3,61 @@ import { AppText } from '@/components/AppText';
 import { Pressable, TextInput, View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
 
+
 export default function EnterEmailScreen() {
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
-        <AppText
-          variant="sectionTitle"
-          color="primary"
-          style={{ textAlign: 'left',paddingLeft: 10 }}
-        >
-          Reset Your Password 🔑
-        </AppText>
-        <AppText
-          variant="body"
-          style={{ textAlign: 'left', marginTop: 20, marginLeft: 5, paddingLeft: 5 }}
-        >
-          Please enter your email address below and we will send an OTP code to
-          reset your password.
-        </AppText>
+        <View>
+          <AppText
+            variant="sectionTitle"
+            color="primary"
+            style={{ textAlign: 'left',paddingLeft:5 }}
+          >
+            Reset Your Password 🔑
+          </AppText>
+          <AppText
+            variant="body"
+            style={{
+              textAlign: 'left',
+              marginTop: 20,
+              marginLeft: 5,
+              paddingLeft: 5,
+            }}
+          >
+            Please enter your email address below and we will send an OTP code
+            to reset your password.
+          </AppText>
+        </View>
 
         <View>
           <AppText
             variant="listHeader"
-            style={{ paddingTop: 50, paddingBottom:10, marginLeft: 10, textAlign: 'left' }}
+            style={{
+              paddingTop: 50,
+              paddingBottom: 10,
+              marginLeft: 10,
+              textAlign: 'left',
+            }}
           >
             {' '}
             Email
           </AppText>
-
-          <TextInput style={styles.input} placeholder='example@gmail.com'/>
+          <View>
+            <TextInput style={styles.input} placeholder='example@gmail.com'/>
+            {/* <AppText variant='bodySecondary' style={{ marginLeft: 25,marginBottom: 10 }}>
+              example@gmail.com
+            </AppText> */}
+          </View>
         </View>
       </View>
 
       <View style={styles.buttonContainer}>
         <Pressable style={styles.button}>
-          <AppText variant='listHeader' style={{color: "#FFFFFF"}}> Continue </AppText>
+          <AppText variant="listHeader" color = "secondary" >
+            {' '}
+            Continue{' '}
+          </AppText>
         </Pressable>
       </View>
     </View>
@@ -47,25 +67,32 @@ export default function EnterEmailScreen() {
 const styles = StyleSheet.create((theme, rt) => ({
   container: {
     flex: 1,
-    backgroundColor: "#FFFFFF",
-    paddingTop: 100,
+    backgroundColor: theme.surface.primary,
+    paddingTop: rt.insets.top,
+    paddingBottom: rt.insets.bottom,
   },
   contentContainer: {
-    flex: 1,
     justifyContent: 'flex-start',
-    paddingTop: 20,
+    paddingTop: rt.insets.top + 30,
+    paddingLeft: rt.insets.left + 10,
+    paddingRight: rt.insets.right + 10,
   },
   buttonContainer: {
-    paddingBottom: 60,
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingBottom: 20,
   },
   input: {
-    borderWidth: 1,
-    borderColor: 'grey',
-    borderRadius: 6,
+    backgroundColor: theme.surface.secondary,
+    //borderWidth: 1,
+    // borderColor: 'grey',
+    borderRadius: 12,
     padding: 10,
     marginTop: 2,
     marginLeft: 10,
     marginRight: 10,
+    // Shadow for Android
+    elevation: 3,
   },
   button: {
     alignItems: 'center',
@@ -74,6 +101,6 @@ const styles = StyleSheet.create((theme, rt) => ({
     marginLeft: 10,
     marginRight: 10,
     borderRadius: 25,
-    backgroundColor: "#9500FF",
-  }
+    backgroundColor: theme.action.secondary,
+  },
 }));
