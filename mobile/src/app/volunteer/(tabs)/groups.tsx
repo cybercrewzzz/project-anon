7193cb70b3a67402274356a3f188b3f58079c1b6
Thusiru <147712158+thusiru@users.noreply.github.com>
2026-02-09 11:51:20 +0530

    fix: Format issues

--- mobile/src/app/volunteer/(tabs)/groups.tsx
+++ mobile/src/app/volunteer/(tabs)/groups.tsx
@@ -1,12 +1,12 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
 
 const Groups = () => {
   return (
     <View>
       <Text>Groups</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Groups
+export default Groups;
