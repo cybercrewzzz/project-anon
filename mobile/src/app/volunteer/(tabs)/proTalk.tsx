e9c7095ae2ad1b874376f17c434f7d95c628f30a
ShadowReaper404 <thevinduisthujaya@gmail.com>
2026-02-10 11:47:17 +0530

    feat: Add ProTalk screen with navigation to p2p-And

--- mobile/src/app/volunteer/(tabs)/proTalk.tsx
+++ mobile/src/app/volunteer/(tabs)/proTalk.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import React from 'react';
+import { router } from 'expo-router';
 
 const ProTalk = () => {
   return (
     <View>
       <Text>ProTalk</Text>
+      <Button title="To the p2p-And screen" onPress={() => router.push('/volunteer/p2p-and')} />
     </View>
   );
 };
