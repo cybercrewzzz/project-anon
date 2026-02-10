930fae169824d2d3c09a6fbd66d477c7c4f1716d
Thusiru <147712158+thusiru@users.noreply.github.com>
2026-02-10 15:22:28 +0530

    Initialise signIn.tsx

--- mobile/src/app/user/(tabs)/groups.tsx
+++ mobile/src/app/user/(tabs)/groups.tsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native';
+import { View, Pressable } from 'react-native';
 import React from 'react';
+import { AppText } from '@/components/AppText';
+import { useRouter } from 'expo-router';
+import { StyleSheet } from 'react-native-unistyles';
 
 const Groups = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Groups</Text>
+    <View style={styles.screen}>
+      <Pressable onPress={() => router.navigate('/user/authScreens/signIn')}>
+        <AppText>Sign In</AppText>
+      </Pressable>
     </View>
   );
 };
 
 export default Groups;
+
+const styles = StyleSheet.create(theme => ({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}));
