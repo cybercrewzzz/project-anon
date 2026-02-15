e40406769b3f6485553566f05c54584ccacbfdbd
Oshadhi <oshadhiranawaka@gmail.com>
2026-02-15 10:08:23 +0530

    refactor: Remove unused imports and button from ProTalk component

--- mobile/src/app/user/(tabs)/proTalk.tsx
+++ mobile/src/app/user/(tabs)/proTalk.tsx
@@ -1,15 +1,12 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text} from 'react-native';
 import React from 'react';
-import { router } from 'expo-router';
 
 const ProTalk = () => {
   return (
     <View>
       <Text>ProTalk</Text>
-      <Button title="To the p2p-And screen" onPress={() => router.push('/user/authScreens/OTPVerification')} />
     </View>
   );
 };
-
 
 export default ProTalk;
