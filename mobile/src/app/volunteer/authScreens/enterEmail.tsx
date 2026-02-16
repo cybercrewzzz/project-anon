de9ed9ecf3971685c77be66eb132bfa0d960fe61
ubddprasanna <ubddprasannapro@gmail.com>
2026-02-16 15:21:45 +0530

    Refactor EnterEmail component for password reset: enhance layout, update text styles, and replace TextInput with InputForm

--- mobile/src/app/volunteer/authScreens/enterEmail.tsx
+++ mobile/src/app/volunteer/authScreens/enterEmail.tsx
@@ -1,50 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppText } from '@/components/AppText';
-import { Pressable, TextInput, View } from 'react-native';
+import { View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
-import { typography } from '@/theme/tokens/typography';
-//import { useRouter } from 'expo-router';
+import { FullWidthButton } from '@/components/FullWidthButton';
+import InputForm from '@/components/inputForm';
 
 const EnterEmail = () => {
-  //const router = useRouter();
+  const [email, setEmail] = useState('');
 
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
         <View>
-          <AppText variant="title1" color="primary" style={styles.title}>
-            Reset Your Password 🔑
+          <AppText
+            variant="title1"
+            emphasis="emphasized"
+            color="primary"
+            style={styles.title}
+          >
+            Reset Your Password
           </AppText>
-          <AppText variant="body" style={styles.description}>
+          <AppText variant="subhead" textAlign="left" style={styles.description}>
             Please enter your email address below and we will send an OTP code
             to reset your password.
           </AppText>
         </View>
 
-        <View>
-          <AppText variant="headline" style={styles.inputName}>
-            {' '}
-            Email
-          </AppText>
-          <View>
-            <TextInput style={styles.input} placeholder="example@gmail.com" />
-            {/* <AppText variant='bodySecondary' style={{ marginLeft: 25,marginBottom: 10 }}>
-              example@gmail.com
-            </AppText> */}
-          </View>
+        <View style={styles.inputSection}>
+          <InputForm
+            placeholder="Email"
+            placeholderColor="subtle2"
+            formColor="#FDFAFF"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoComplete="email"
+          />
         </View>
       </View>
 
       <View style={styles.buttonContainer}>
-        <Pressable
-          style={styles.button}
-          //onPress={() => router.navigate('')}
-        >
+        <FullWidthButton>
           <AppText variant="headline" color="secondary">
-            {' '}
-            Continue{' '}
+            Continue
           </AppText>
-        </Pressable>
+        </FullWidthButton>
       </View>
     </View>
   );
@@ -56,55 +57,30 @@ const styles = StyleSheet.create((theme, rt) => ({
   container: {
     flex: 1,
     backgroundColor: theme.surface.primary,
-    paddingTop: rt.insets.top,
+    paddingTop: rt.insets.top+ theme.spacing.s6,
     paddingBottom: rt.insets.bottom,
-    paddingLeft: rt.insets.left + 16,
-    paddingRight: rt.insets.right + 16,
+    paddingLeft: rt.insets.left + theme.spacing.s4,
+    paddingRight: rt.insets.right + theme.spacing.s4,
   },
   contentContainer: {
     justifyContent: 'flex-start',
-    paddingTop: rt.insets.top + 30,
+    paddingTop: rt.insets.top + theme.spacing.s6,
   },
   buttonContainer: {
     flex: 1,
     justifyContent: 'flex-end',
-    paddingBottom: 64,
+    paddingBottom: theme.spacing.s8,
   },
   title: {
     textAlign: 'left',
-    paddingLeft: 5,
-    fontWeight: typography.title1.emphasized.fontWeight,
+    paddingLeft: theme.spacing.s2,
   },
   description: {
-    textAlign: 'left',
-    marginTop: 20,
-    marginLeft: 5,
-    paddingLeft: 5,
+    marginTop: theme.spacing.s5,
+    marginLeft: theme.spacing.s2,
+    paddingLeft: theme.spacing.s2,
   },
-  inputName: {
-    paddingTop: 50,
-    paddingBottom: 10,
-    marginLeft: 10,
-    textAlign: 'left',
-  },
-  input: {
-    backgroundColor: theme.surface.secondary,
-    height: 44,
-    borderRadius: 12,
-    padding: 10,
-    marginTop: 2,
-    marginLeft: 10,
-    marginRight: 10,
-    // Shadow for Android
-    elevation: 2,
-  },
-  button: {
-    alignItems: 'center',
-    paddingTop: 15,
-    paddingBottom: 15,
-    marginLeft: 10,
-    marginRight: 10,
-    borderRadius: 25,
-    backgroundColor: theme.action.secondary,
+  inputSection: {
+    paddingTop: theme.spacing.s7,
   },
 }));
