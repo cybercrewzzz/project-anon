61bc6e48765676be403fc51a54cdd41ea459450e
Thusiru <147712158+thusiru@users.noreply.github.com>
2026-02-16 13:36:11 +0530

    Fix minor issues

--- mobile/src/app/user/TnS agree/TnS.tsx
+++ mobile/src/app/user/TnS agree/TnS.tsx
@@ -2,38 +2,20 @@ import { AppText } from '@/components/AppText';
 import { Pressable, View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
 import { router } from 'expo-router';
-import { purple } from '@/theme/palettes/purple';
 
 export default function TnS() {
   return (
     <View style={styles.container}>
-      <View style={{ justifyContent: 'flex-start' }}>
-        <AppText
-          variant="largeTitle"
-          color="primary"
-          style={{
-            textAlign: 'center',
-            paddingTop: 80,
-            color: purple[600],
-            fontSize: 26,
-            fontWeight: 'bold',
-          }}
-        >
-          Terms & Conditions
-        </AppText>
-      </View>
-
-      <Pressable
-        style={{
-          backgroundColor: purple[700],
-          padding: 15,
-          borderRadius: 25,
-          width: 300,
-          alignItems: 'center',
-          marginTop: 510,
-        }}
-        onPress={() => router.push('/')}
+      <AppText
+        variant="title1"
+        emphasis="emphasized"
+        color="subtle1"
+        textAlign="center"
       >
+        Terms & Conditions
+      </AppText>
+
+      <Pressable style={styles.textContainer} onPress={() => router.push('/')}>
         <AppText style={{ color: '#FFFFFF', fontSize: 16, fontWeight: 'bold' }}>
           Agree & Continue
         </AppText>
@@ -48,12 +30,15 @@ const styles = StyleSheet.create((theme, rt) => ({
     justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: theme.background.default,
-    marginTop: rt.insets.top,
+    paddingTop: rt.insets.top + 80,
     gap: 50,
   },
   textContainer: {
-    flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#570096',
+    padding: 15,
+    borderRadius: 25,
+    width: 300,
     alignItems: 'center',
+    marginTop: 510,
   },
 }));
