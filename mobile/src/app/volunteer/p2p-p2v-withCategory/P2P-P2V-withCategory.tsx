8302c14ccd18f7bc3af5b3b1dff4beb13467d925
Oshadhi <oshadhiranawaka@gmail.com>
2026-02-27 13:03:32 +0530

    Add navigation button to P2P-P2V-withCategory and create layout component

---
+++ mobile/src/app/volunteer/p2p-p2v-withCategory/P2P-P2V-withCategory.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View} from "react-native";
+
+const P2P_P2V_withCategory = () => {
+  return (
+      <View>
+
+        </View>
+    );
+};
+
+export default P2P_P2V_withCategory;
+
+
