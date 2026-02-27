b68ecec6944a96558a590bcb204951120b719af5
Oshadhi <oshadhiranawaka@gmail.com>
2026-02-27 19:04:40 +0530

    Add Feeling description cards and plus icon to add more

--- mobile/src/app/volunteer/p2p-p2v-withCategory/P2P-P2V-withCategory.tsx
+++ mobile/src/app/volunteer/p2p-p2v-withCategory/P2P-P2V-withCategory.tsx
@@ -38,11 +38,111 @@ const P2P_P2V_withCategory = () => {
             5{' '}
           </AppText>
         </View>
-          </View>
+      </View>
 
-          <View >
+      <View style={styles.emotionCard}>
+        <AppText
+          variant="body"
+          emphasis="emphasized"
+          style={styles.emotionCardText}
+        >
+          {' '}
+          How are you feeling Right Now?{' '}
+        </AppText>
+        <View style={styles.emojeeContainer}>
+          <Image
+            source={require('@/assets/icons/face-em1.svg')}
+            style={{ width: 35, height: 35 }}
+          ></Image>
+          <Image
+            source={require('@/assets/icons/face-em2.svg')}
+            style={{ width: 35, height: 35 }}
+          ></Image>
+          <Image
+            source={require('@/assets/icons/face-em3.svg')}
+            style={{ width: 35, height: 35 }}
+          ></Image>
+          <Image
+            source={require('@/assets/icons/face-em4.svg')}
+            style={{ width: 35, height: 35 }}
+          ></Image>
+          <Image
+            source={require('@/assets/icons/face-em5.svg')}
+            style={{ width: 35, height: 35 }}
+          ></Image>
+        </View>
+      </View>
 
+      <View style={styles.categoryCard}>
+        <AppText
+          variant="body"
+          emphasis="emphasized"
+          style={styles.emotionCardText}
+        >
+          {' '}
+          What is troubling you today?{' '}
+        </AppText>
+        <View style={styles.categoryType}>
+          <AppText
+            variant="body"
+            emphasis="emphasized"
+            style={styles.categoryTypeText}
+          >
+            {' '}
+            Family Problem{' '}
+          </AppText>
+          <Image
+            source={require('@/assets/icons/chevron-downOPT.svg')}
+            style={styles.dropDownIcon}
+          ></Image>
+        </View>
+        <View style={styles.fellingDescriptionContainer}>
+          <AppText
+            variant="body"
+            emphasis="emphasized"
+            style={styles.emotionCardText}
+          >
+            {' '}
+            What Best Describes this Feeling?{' '}
+          </AppText>
+
+          <View style={styles.descriptionCardContainer}>
+            <View style={styles.pointCard}>
+              <AppText
+                variant="caption1"
+                emphasis="emphasized"
+                style={styles.pointText}
+              >
+                Anxiety
+              </AppText>
+            </View>
+            <View style={styles.pointCard}>
+              <AppText
+                variant="caption1"
+                emphasis="emphasized"
+                style={styles.pointText}
+              >
+                Stress
+              </AppText>
+            </View>
+            <View style={styles.pointCard}>
+              <AppText
+                variant="caption1"
+                emphasis="emphasized"
+                style={styles.pointText}
+              >
+                Depression
+              </AppText>
+            </View>
+            <View style={styles.pointCard}>
+              <Image
+                source={require('@/assets/icons/plusIconOPT.svg')}
+                style={{ width: 17, height: 16 }}
+              ></Image>
+            </View>
           </View>
+        </View>
+      </View>
     </View>
   );
 };
@@ -92,5 +192,60 @@ const styles = StyleSheet.create((theme, rt) => ({
   ticketText: {
     color: theme.text.accent,
     paddingHorizontal: 5,
+  },
+  emotionCard: {
+    backgroundColor: theme.surface.primary,
+    borderRadius: theme.radius.md,
+    padding: theme.spacing.s4,
+    marginTop: 20,
+  },
+  emotionCardText: {
+    color: theme.text.subtle1,
+    margin: 5,
+  },
+  emojeeContainer: {
+    flexDirection: 'row',
+    marginTop: 10,
+    gap: 32,
+    padding: 4,
+  },
+  categoryCard: {
+    backgroundColor: theme.surface.primary,
+    borderRadius: theme.radius.md,
+    padding: theme.spacing.s4,
+    marginTop: 20,
+  },
+  categoryType: {
+    flexDirection: 'row',
+    marginTop: 10,
+    backgroundColor: theme.surface.secondary,
+    borderColor: theme.text.subtle2,
+    borderWidth: 2,
+    borderRadius: theme.radius.md,
+  },
+  categoryTypeText: {
+    color: theme.text.primary,
+    paddingVertical: 8,
+    paddingHorizontal: 4,
+  },
+  dropDownIcon: {
+    width: 16,
+    height: 14,
+    alignSelf: 'center',
+    marginRight: 8,
+    marginLeft: 'auto',
+  },
+  fellingDescriptionContainer: {
+    marginTop: 10,
+    backgroundColor: theme.surface.secondary,
+    borderColor: theme.text.subtle2,
+    borderWidth: 2,
+    borderRadius: theme.radius.md,
+  },
+  descriptionCardContainer: {
+    flexDirection: 'row',
+    marginTop: 4,
+    gap: 16,
+    padding: 10,
   },
 }));
