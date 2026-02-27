58fcf6981def2bf6d1a07fef31d2869ff231fdd8
Oshadhi <oshadhiranawaka@gmail.com>
2026-02-27 12:08:51 +0530

    Enhance Login Successful screen with image and improved layout

--- mobile/src/app/volunteer/loginSuccessful/LoginSuccessful.tsx
+++ mobile/src/app/volunteer/loginSuccessful/LoginSuccessful.tsx
@@ -1,28 +1,58 @@
 import { AppText } from '@/components/AppText';
 import React from 'react';
 import { View } from 'react-native';
+import { Image } from 'expo-image';
 import { StyleSheet } from 'react-native-unistyles';
 
 const LoginSuccessful = () => {
   return (
     <View style={styles.container}>
-          <View style={styles.cardContainer}>
-              <View >
-                  <AppText variant="title3" color='primary' emphasis='emphasized' style={{justifyContent: 'center', textAlign: 'center'}}>
-                Login Successful!
-              </AppText>
-              <AppText variant="subhead" color='primary' style={{ paddingTop: 32, justifyContent: 'center', textAlign: 'center' }}>
-                  Please Wait
-              </AppText>
-              <AppText variant='subhead' color='primary' style={{
-                  paddingTop: 8, marginTop: 8, justifyContent: 'center', textAlign: 'center'
-              }}>
-                  You will be directed to the homepage.
-              </AppText>
-
-              </View>
-
-
+      <View style={styles.cardContainer}>
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            paddingBottom: 32,
+          }}
+        >
+          <Image
+            source={require('@/assets/images/loginSuccessful.webp')}
+            style={{ width: 200, height: 185 }}
+          />
+        </View>
+        <View>
+          <AppText
+            variant="title3"
+            color="primary"
+            emphasis="emphasized"
+            style={{ justifyContent: 'center', textAlign: 'center' }}
+          >
+            Login Successful!
+          </AppText>
+          <AppText
+            variant="subhead"
+            color="primary"
+            style={{
+              paddingTop: 32,
+              justifyContent: 'center',
+              textAlign: 'center',
+            }}
+          >
+            Please Wait
+          </AppText>
+          <AppText
+            variant="subhead"
+            color="primary"
+            style={{
+              paddingTop: 8,
+              marginTop: 8,
+              justifyContent: 'center',
+              textAlign: 'center',
+            }}
+          >
+            You will be directed to the homepage.
+          </AppText>
+        </View>
       </View>
     </View>
   );
