3ba6431a92974f7fd0ec7edd1540c2a01bb601c4
Thevindu Sithujaya <thevindusithujaya@gmail.com>
2026-03-04 22:32:06 +0530

    Refactor ProTalk component to use SafeAreaView and apply consistent styling

--- mobile/src/app/volunteer/(tabs)/proTalk.tsx
+++ mobile/src/app/volunteer/(tabs)/proTalk.tsx
@@ -1,34 +1,48 @@
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { router } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const ProTalk = () => {
   return (
-    <View>
-      <Text>ProTalk</Text>
-      <Button
-        title="To the OTPVerification"
-        onPress={() => router.push('/volunteer/authScreens/OTPVerification')}
-      />
-      <Button
-        title="To Enter Email"
-        onPress={() => router.push('/volunteer/authScreens/enterEmail')}
-      />
-      <Button
-        title="To Login Successful"
-        onPress={() =>
-          router.push('/volunteer/loginSuccessful/LoginSuccessful')
-        }
-      />
-      <Button
-        title="To Sign Up and Login"
-        onPress={() => router.navigate('/volunteer/signUpNlogin/SignUpNLogin')}
-      />
-      <Button
-        title="P2P AND"
-        onPress={() => router.navigate('/volunteer/P2p-And/p2p-and')}
-      />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text>ProTalk</Text>
+        <Button
+          title="To the OTPVerification"
+          onPress={() => router.push('/volunteer/authScreens/OTPVerification')}
+        />
+        <Button
+          title="To Enter Email"
+          onPress={() => router.push('/volunteer/authScreens/enterEmail')}
+        />
+        <Button
+          title="To Login Successful"
+          onPress={() =>
+            router.push('/volunteer/loginSuccessful/LoginSuccessful')
+          }
+        />
+        <Button
+          title="To Sign Up and Login"
+          onPress={() =>
+            router.navigate('/volunteer/signUpNlogin/SignUpNLogin')
+          }
+        />
+        <Button
+          title="P2P AND"
+          onPress={() => router.navigate('/volunteer/P2p-And/p2p-and')}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default ProTalk;
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+  },
+});
