f4d17162c65a118015e0c2284d153ab3ddd1905a
Thusiru <147712158+thusiru@users.noreply.github.com>
2026-03-05 17:33:22 +0530

    Refactor prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from './prisma/prisma.service';
 import { UsersService } from './user.service';
 import { PostsService } from './post.service';
 
