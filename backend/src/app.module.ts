aac8b6f3c8c3d32a0c39958980db18286aed4f26
Thusiru <147712158+thusiru@users.noreply.github.com>
2026-03-05 14:05:55 +0530

    Test prisma with mock db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
+import { UserService } from './user.service';
+import { PostService } from './post.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService, PostService],
 })
 export class AppModule {}
