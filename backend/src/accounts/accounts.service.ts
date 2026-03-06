4f89470ba18dcb1046a5a87040814d9fef60f04f
Thusiru <147712158+thusiru@users.noreply.github.com>
2026-03-06 21:44:22 +0530

    Automate scripts

---
+++ backend/src/accounts/accounts.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Account } from '../generated/prisma/client';
+
+@Injectable()
+export class AccountsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(): Promise<Account[]> {
+    return this.prisma.account.findMany();
+  }
+}
