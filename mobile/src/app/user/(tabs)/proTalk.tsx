10c87ca4842fed5d7f76ccfc7d96c2c5ad6b5fe5
Thevindu Sithujaya <thevindusithujaya@gmail.com>
2026-03-06 10:10:55 +0530

    feat: wrap ProTalk component in SafeAreaView for improved layout

--- mobile/src/app/user/(tabs)/proTalk.tsx
+++ mobile/src/app/user/(tabs)/proTalk.tsx
@@ -5,45 +5,47 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 const ProTalk = () => {
   return (
-    <View>
-      <Text>ProTalk</Text>
-      <Button
-        title="TnS Agreee"
-        onPress={() => router.push('/user/TnS agree/TnS')}
-      />
-      <Button
-        title="Special Notice"
-        onPress={() => router.push('/user/Special Notice/specialnotice')}
-      />
-      <Button
-        title="Peer to Peer"
-        onPress={() => router.push('/user/PeertoPeer/peertopeer')}
-      />
-      <Button
-        title="To User Profile"
-        onPress={() => router.push('/user/userProfile/UserProfile')}
-      />
-      <Button
-        title="To Waiting Screen"
-        onPress={() => router.push('/user/WaitingScreen/waitingScreen')}
-      />
-      <Button
-        title="To P2P-P2V-withCategory"
-        onPress={() =>
-          router.navigate('/user/p2p-p2v-withCategory/P2P-P2V-withCategory')
-        }
-      />
-      <Button
-        title="To Category Dropdown Filter"
-        onPress={() =>
-          router.push('/user/categorydropdownfilter/categorydropdownfilter')
-        }
-      />
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text>ProTalk</Text>
+        <Button
+          title="TnS Agreee"
+          onPress={() => router.push('/user/TnS agree/TnS')}
+        />
+        <Button
+          title="Special Notice"
+          onPress={() => router.push('/user/Special Notice/specialnotice')}
+        />
+        <Button
+          title="Peer to Peer"
+          onPress={() => router.push('/user/PeertoPeer/peertopeer')}
+        />
+        <Button
+          title="To User Profile"
+          onPress={() => router.push('/user/userProfile/UserProfile')}
+        />
+        <Button
+          title="To Waiting Screen"
+          onPress={() => router.push('/user/WaitingScreen/waitingScreen')}
+        />
+        <Button
+          title="To P2P-P2V-withCategory"
+          onPress={() =>
+            router.navigate('/user/p2p-p2v-withCategory/P2P-P2V-withCategory')
+          }
+        />
+        <Button
+          title="To Category Dropdown Filter"
+          onPress={() =>
+            router.push('/user/categorydropdownfilter/categorydropdownfilter')
+          }
+        />
         <Button
           title="Category Dropdown 1"
           onPress={() => router.navigate('/user/categorydropdown1/categorydrop1')}
         />
-    </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
