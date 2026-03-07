ac2cc886667795946c72dc7d767353aa90640d20
Thusiru <147712158+thusiru@users.noreply.github.com>
2026-03-07 15:44:25 +0530

    Fix an eslint issue + lint issues

--- mobile/eslint.config.js
+++ mobile/eslint.config.js
@@ -2,7 +2,7 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 const eslintConfigPrettier = require('eslint-config-prettier/flat');
-import pluginQuery from '@tanstack/eslint-plugin-query';
+const pluginQuery = require('@tanstack/eslint-plugin-query');
 
 module.exports = defineConfig([
   ...pluginQuery.configs['flat/recommended'],
