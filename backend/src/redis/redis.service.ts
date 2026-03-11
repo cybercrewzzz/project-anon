d4042b60498debfdde980d74ef8e5482d711a3e8
Thusiru <147712158+thusiru@users.noreply.github.com>
2026-03-11 13:23:25 +0530

    Fix a redis issue

--- backend/src/redis/redis.service.ts
+++ backend/src/redis/redis.service.ts
@@ -1,11 +1,10 @@
 import { Injectable, OnModuleDestroy } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService extends Redis implements OnModuleDestroy {
-  constructor(configService: ConfigService) {
-    super(configService.get<string>('REDIS_URL', 'redis://localhost:6379'));
+  constructor() {
+    super(process.env.REDIS_URL ?? 'redis://localhost:6379');
   }
 
   async onModuleDestroy() {
