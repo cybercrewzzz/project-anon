103cca17d435d7e59bb9b2824b6082094dbc753e
Thusiru <147712158+thusiru@users.noreply.github.com>
2026-03-12 15:32:31 +0530

    Remove accounts example

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { AccountsModule } from './accounts/accounts.module';
 import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PrismaModule, AccountsModule, AdminModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
