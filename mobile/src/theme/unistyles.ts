6cb33164309210b3ac8b86c9108ab4879fa3e403
ubddprasanna <ubddprasannapro@gmail.com>
2026-03-15 13:07:21 +0530

    feat: add cyan color palette and volunteer light theme configuration

--- mobile/src/theme/unistyles.ts
+++ mobile/src/theme/unistyles.ts
@@ -1,14 +1,17 @@
 import { StyleSheet } from 'react-native-unistyles';
 import { AppTheme, userLightTheme } from './roles/user/light';
+import { VolunteerTheme, volunteerLightTheme } from './roles/volunteer/light';
 
 declare module 'react-native-unistyles' {
   export interface UnistylesThemes {
     userLight: AppTheme;
+    volunteerLight: VolunteerTheme;
   }
 }
 
 StyleSheet.configure({
   themes: {
     userLight: userLightTheme,
+    volunteerLight: volunteerLightTheme,
   },
 });
