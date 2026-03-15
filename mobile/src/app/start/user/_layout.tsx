2c151eb383c97a78b8e7949d7d98310310c10e8c
Thusiru <147712158+thusiru@users.noreply.github.com>
2026-03-15 16:33:20 +0530

    Add role change logics to relevant layout files

--- mobile/src/app/start/user/_layout.tsx
+++ mobile/src/app/start/user/_layout.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Stack } from 'expo-router';
+import { useRole } from '@/store/useRole';
 
 const StartLayout = () => {
+  const setRole = useRole(state => state.setRole);
+
+  useEffect(() => {
+    setRole('user');
+  }, [setRole]);
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="welcome" />
--- mobile/src/app/start/volunteer/_layout.tsx
+++ mobile/src/app/start/volunteer/_layout.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Stack } from 'expo-router';
+import { useRole } from '@/store/useRole';
 
 const StartLayout = () => {
+  const setRole = useRole(state => state.setRole);
+
+  useEffect(() => {
+    setRole('volunteer');
+  }, [setRole]);
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="welcome" />
