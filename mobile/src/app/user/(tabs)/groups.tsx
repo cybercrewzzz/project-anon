21324fc38bba300efa3f371bcf999849868ea5dc
ubddprasanna <ubddprasannapro@gmail.com>
2026-03-15 17:49:23 +0530

    format the whole branch

--- mobile/src/app/user/(tabs)/groups.tsx
+++ mobile/src/app/user/(tabs)/groups.tsx
@@ -11,7 +11,7 @@ const Groups = () => {
     <View style={styles.screen}>
       <Button
         text="Get Started"
-        onPress={() => router.push('/start/user/welcome' as any)}
+        onPress={() => router.push('/start/welcome' as any)}
       />
       <Button
         text="Sign In"
