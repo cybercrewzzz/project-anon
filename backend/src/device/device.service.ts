9045c2d2f0c4586780e1694226ab4b4252a42fd7
Senithi Premaratne <{email}>
2026-03-27 13:05:13 +0530

    removed import platform

--- backend/src/device/device.service.ts
+++ backend/src/device/device.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { RegisterDeviceTokenDto } from './dto/register-device-token.dto';
-import { Platform } from '../generated/prisma/client';
 
 @Injectable()
 export class DeviceService {
