3a40dca372d73848507a10c8ca2d5735c12c1f4b
Thevindu Sithujaya <thevindusithujaya@gmail.com>
2026-04-09 00:26:19 +0530

    Remove unused import for Max validator in RegisterVolunteerDto

--- backend/src/auth/dto/register-volunteer.dto.ts
+++ backend/src/auth/dto/register-volunteer.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString, Max, MaxLength, MinLength } from 'class-validator';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class RegisterVolunteerDto {
   @IsString()
